3de01f5711be512ae245357e49b534f5bdd7eb23
CapitaineCouscous <103769157+CapitaineCouscous@users.noreply.github.com>
2022-09-17 22:19:55 +0200

    Ajout fonction total
    total(main) -> renvoie le total des valeurs des cartes de main en prenant compte du cas de l'as

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,37 @@
+import random as rd
 
+#Constantes
+liste_faces = ['1','2','3','4','5','6','7','8','9','10','V','D','R']
+liste_signes = ['♥','♦','♣','♠']
+
+#Fonctions
+"""
+carte = une liste sous la forme [('face','signe'),'valeur']
+main = une liste de cartes sous la forme ['carte','carte',...]
+"""
+
+def tirage(nb=2):
+    main = []
+    for _ in range(nb):
+        face = rd.choice(liste_faces)
+        if face == '1':
+            valeur = 11
+        elif face in '2,3,4,5,6,7,8,9':
+            valeur = int(face)
+        else:
+            valeur = 10
+        main.append([(face,rd.choice(liste_signes)),valeur])
+    return main
+
+def total(main):
+    total = 0
+    for carte in main:
+        total += carte[1]
+    if total > 21:
+        for carte in main:
+            if carte[0][0]=='1' and total > 21:
+                carte[1] = 1
+                total -= 10
+            elif total <= 21:
+                break
+    return total
