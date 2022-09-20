ab852ef27c96591d51649a82a93b38b152884a6d
CapitaineCouscous <103769157+CapitaineCouscous@users.noreply.github.com>
2022-09-20 22:22:36 +0200

    Masterisation du bordel.
    Ajout de commentaires et modifications de 2-3 variables.

--- blackjack.py
+++ blackjack.py
@@ -1,61 +1,71 @@
 import random as rd
 
-liste_faces = ['1','2','3','4','5','6','7','8','9','10','V','D','R']
+#listes utilisées pour faire les tirages de cartes
+liste_valeurs = ['1','2','3','4','5','6','7','8','9','10','V','D','R']
 liste_signes = ['♥','♦','♣','♠']
 
 """
-carte = une liste sous la forme [('face','signe'),'valeur']
-main = une liste de cartes sous la forme ['carte','carte',...]
+carte = une liste sous la forme [(valeur,signe),point]
+main = une liste de cartes sous la forme [carte,carte,...]
 """
 
 def tirage(nb=2):
+    """
+    Renvoie une main de nb carte tirée aléatoirement.
+    """
     main = []
     for _ in range(nb):
-        face = rd.choice(liste_faces)
-        if face == '1':
-            valeur = 11
-        elif face in '2,3,4,5,6,7,8,9':
-            valeur = int(face)
+        valeur = rd.choice(liste_valeurs) #donne une valeur aléatoire
+        if valeur == '1': #associe un nombre de point en fonction de la valeur de la carte
+            point = 11
+        elif valeur in '2,3,4,5,6,7,8,9':
+            point = int(valeur)
         else:
-            valeur = 10
-        main.append([(face,rd.choice(liste_signes)),valeur])
+            point = 10
+        main.append([(valeur,rd.choice(liste_signes)),point]) #crée une carte en associant la valeur avec un signe tirée aléatoirement(le signe n'a pas d'utilité à part esthétique) et les points et ajoute cette carte à la main
     return main
 
 def total(main):
+    """
+    Renvoie le total des valeurs des cartes de main et change la valeur des as en 1 en fonction de si le total dépasse 21.
+    """
     total = 0
-    for carte in main:
+    for carte in main: #fait le total des valeurs des cartes de main avec 11 comme valeur de l'as
         total += carte[1]
     if total > 21:
         for carte in main:
-            if carte[0][0]=='1' and total > 21:
+            if carte[0][0]=='1' and total > 21: #si le total est supérieur à 21 et que la carte est un as, remplace la valeur de l'as par 1 et actualise la valeur du total
                 carte[1] = 1
                 total -= 10
-            elif total <= 21:
+            elif total <= 21: #et si le total est inférieur ou égal à 21 la boucle se termine
                 break
     return total
 
 def blackjack():
+    """
+    Lance une partie de Blackjack.
+    """
     jouer = input("Voulez-vous jouer au Blackjack ? [Y/N] ")
     if jouer.lower() == 'y':
-        main_joueur,main_croupier = tirage(),tirage()
-        total_joueur,total_croupier, = total(main_joueur),total(main_croupier)
-        print("Le jeu du croupier :",[main_croupier[0],'Carte cachée'],"\nVotre jeu :",main_joueur,total_joueur)
+        main_joueur,main_croupier = tirage(),tirage() #fait 2 tirage de 2 cartes, pour le joueur et le croupier
+        total_joueur,total_croupier, = total(main_joueur),total(main_croupier) #fait les totaux des mains du joueur et du croupier
+        print("Le jeu du croupier :",[main_croupier[0],'Carte cachée'],"\nVotre jeu :",main_joueur,total_joueur) #affiche les mains et le total de la main du joueur en cachant la 2eme carte du croupier
         if total_joueur == 21:
-            print("Vous gagnez en obtenant un Blackjack !")
+            print("Vous gagnez en obtenant un Blackjack !") #victoire en ayant 21 points dès le premier tirage
         else:
-            suite = input("Que voulez-vous faire ? [O]btenir des cartes/[G]arder ses cartes ")
-            if suite.lower() == 'o':
-                nb_cartes = int(input("Combien de cartes voulez-vous ? "))
+            suite = input("Voulez-vous des cartes supplémentaires ? [Y/N] ")
+            if suite.lower() == 'y':
+                nb_cartes = int(input("Combien de cartes voulez-vous ? [nb] ")) #demande le nombre de cartes supplémentaires voulu par le joueur et les rajoute à la main, puis recalcule le total
                 main_joueur += tirage(nb_cartes)
                 total_joueur = total(main_joueur)
-            while total_croupier < 17:
+            while total_croupier < 17: #rajoute des cartes à la main du croupier jusqu'à ce que le total dépasse ou égalise 17
                 main_croupier += tirage(1)
                 total_croupier = total(main_croupier)
-            print("Le jeu du croupier :",main_croupier,total_croupier,"\nVotre jeu :",main_joueur,total_joueur)
-            if (total_joueur <= 21 and total_joueur > total_croupier) or (total_joueur <= 21 and total_croupier > 21):
+            print("Le jeu du croupier :",main_croupier,total_croupier,"\nVotre jeu :",main_joueur,total_joueur) #affiche les mains et les totaux du croupier et du joueur
+            if total_joueur <= 21 and (total_joueur > total_croupier or total_croupier > 21): #conditions pour gagner
                 print("Vous avez gagné !")
-            elif total_croupier == total_joueur or (total_croupier > 21 and total_joueur > 21):
+            elif total_croupier == total_joueur or (total_croupier > 21 and total_joueur > 21): #conditions si il n'y a pas de gagnant
                 print("Il n'y a aucun gagnant.")
-            else:
+            else: #sinon défaite
                 print("Vous avez perdu.")
     return None
