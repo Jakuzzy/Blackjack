bf38aa67f5e65eb4a561980c10efde8ad1570375
CapitaineCouscous <103769157+CapitaineCouscous@users.noreply.github.com>
2022-09-26 16:20:57 +0200

    Update and rename blackjack.py to blackjack_v2.py

--- blackjack_v2.py
+++ blackjack_v2.py
@@ -60,45 +60,6 @@ def total(main):
 
 
 
-def blackjack():
-    """
-    Lance une partie de Blackjack.
-    """
-    jouer = input("Voulez-vous jouer au Blackjack ? [Y/N] ")
-    if jouer.lower() == 'y':
-        main_joueur,main_croupier = tirage(),tirage() #fait 2 tirage de 2 cartes, pour le joueur et le croupier
-        total_joueur,total_croupier, = total(main_joueur),total(main_croupier) #fait les totaux des mains du joueur et du croupier
-
-        print("\n1er tour :") #affiche les mains en cachant la 2eme carte du croupier et le total de la main du joueur pour le 1er tour
-        print("La main du croupier :",[main_croupier[0],'Carte cachée'],"\nVotre main :",main_joueur,"\n Total de vos cartes :",total_joueur,'\n')
-
-        if total_joueur == 21:
-            print("Vous gagnez en obtenant un Blackjack !") #victoire en ayant 21 points dès le premier tirage
-
-        else:
-            suite = input("Voulez-vous des cartes supplémentaires ? [Y/N] ") #demande si le joueur veut des cartes supplémentaires
-            if suite.lower() == 'y':
-                nb_cartes = int(input("Combien de cartes voulez-vous ? [nb] ")) #demande le nombre de cartes supplémentaires voulu par le joueur et les rajoute à la main, puis recalcule le total
-                main_joueur += tirage(nb_cartes)
-                total_joueur = total(main_joueur)
-
-            while total_croupier < 17: #rajoute des cartes à la main du croupier jusqu'à ce que le total dépasse ou égalise 17
-                main_croupier += tirage(1)
-                total_croupier = total(main_croupier)
-
-            print("\n2eme tour :") #affiche les mains et les totaux du croupier et du joueur pour la fin du jeu
-            print("La main du croupier :",main_croupier,"\n Total des cartes du croupier :",total_croupier,"\nVotre main :",main_joueur,"\n Total de vos cartes :",total_joueur)
-
-            if total_joueur <= 21 and (total_joueur > total_croupier or total_croupier > 21): #conditions pour gagner
-                print("\nVous avez gagné !")
-            elif total_croupier == total_joueur or (total_croupier > 21 and total_joueur > 21): #conditions si il n'y a pas de gagnant
-                print("\nIl n'y a aucun gagnant.")
-            else: #sinon défaite
-                print("\nVous avez perdu.")
-    return None
-
-
-
 def affichage(main,cache=False):
     """
     Renvoie une chaine de caractère pour afficher les cartes dans la console.
